9253887419936c3b1093aa9351248cf7d223b21c
Luiz Rioja <lulyrioja@gmail.com>
2022-01-16 16:23:09 -0300

    :alien: Creating urls and views for schedules, scheduling visits and canceling schedules

--- aplication/views.py
+++ aplication/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http.response import HttpResponse
-from .models import Imovel, Cidade
-from django.shortcuts import render, get_object_or_404
+from .models import Imovel, Cidade, Visitas
 
 
 @login_required(login_url='/auth/logar/')
@@ -35,3 +35,34 @@ def imovel(request, id):
     sugestoes = Imovel.objects.filter(cidade=imovel.cidade).exclude(id=id)[:2]
 
     return render(request, 'imovel.html', {'imovel': imovel, 'sugestoes': sugestoes, 'id': id})
+
+
+def agendar_visitas(req):
+    usuario = req.user
+    dia = req.POST.get('dia')
+    horario = req.POST.get('horario')
+    id_imovel = req.POST.get('id_imovel')
+
+    visita = Visitas(
+        imovel_id=id_imovel,
+        usuario=usuario,
+        dia=dia,
+        horario=horario
+    )
+    visita.save()
+
+    return redirect('/agendamentos')
+
+
+def agendamentos(req):
+    visitas = Visitas.objects.filter(usuario=req.user)
+
+    return render(req, "agendamentos.html", {'visitas': visitas})
+
+
+def cancelar_agendamento(req, id):
+    visitas = get_object_or_404(Visitas, id=id)
+    visitas.status = "C"
+    visitas.save()
+
+    return redirect('/agendamentos')
